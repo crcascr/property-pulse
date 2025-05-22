4d3195a2fd39fe2dcef408a65d508bd9bd688d9c
crcascr <cristianj_20@hotmail.com>
2025-05-21 19:08:41 -0500

    feat(auth): add sign out function to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,6 +15,7 @@ import {
   LiteralUnion,
   signIn,
   useSession,
+  signOut,
 } from "next-auth/react";
 import { BuiltInProviderType } from "next-auth/providers/index";
 
@@ -22,8 +23,6 @@ export const Navbar = () => {
   const pathname = usePathname();
 
   const { data: session } = useSession();
-
-  console.log(session);
 
   const profilePicture = session?.user?.image;
 
@@ -67,7 +66,7 @@ export const Navbar = () => {
             <button
               type="button"
               id="mobile-dropdown-button"
-              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              className="relative cursor-pointer inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
               onClick={() => setMobileMenuOpen((prev) => !prev)}
@@ -110,7 +109,7 @@ export const Navbar = () => {
                 {providers &&
                   Object.values(providers).map((provider, index) => (
                     <button
-                      className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                      className="flex items-center cursor-pointer text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                       onClick={() => signIn(provider.id)}
                       key={index}
                     >
@@ -127,7 +126,7 @@ export const Navbar = () => {
               <Link href="/messages" className="relative group">
                 <button
                   type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  className="relative cursor-pointer rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
                   <span className="absolute -inset-1.5"></span>
                   <span className="sr-only">View notifications</span>
@@ -142,7 +141,7 @@ export const Navbar = () => {
                 <div>
                   <button
                     type="button"
-                    className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    className="relative cursor-pointer flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                     id="user-menu-button"
                     aria-expanded="false"
                     aria-haspopup="true"
@@ -189,10 +188,14 @@ export const Navbar = () => {
                       Saved Properties
                     </Link>
                     <button
-                      className="block px-4 py-2 text-sm text-gray-700"
+                      className="block px-4 py-2 text-sm text-gray-700 cursor-pointer"
                       role="menuitem"
                       tabIndex={-1}
                       id="user-menu-item-2"
+                      onClick={() => {
+                        setProfileMenuOpen(false);
+                        signOut();
+                      }}
                     >
                       Sign Out
                     </button>
